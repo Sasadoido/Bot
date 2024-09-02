ec5125a2cf146deeb7d8f11c79d71e77de072697
Sasadoido <alex.cristian.roncalli@gmail.com>
2024-09-02 20:27:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,77 +2,78 @@ import streamlit as st
 import replicate
 import os
 
-# App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+#tradutor
+from deep_translator import GoogleTranslator
 
-# Replicate Credentials
+# Titulo
+st.set_page_config(page_title="PsicoBoto")
+
+# Autentificar as credenciais
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
+    st.title('💬 PsicoBot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        st.success('API key correta!', icon='✅')
+        replicate_api = st.secrets["REPLICATE_API_TOKEN"]
+        client = replicate.Client(api_token=replicate_api)
+        #os.environ["REPLICATE_API_TOKEN"] = replicate_api
+        #replicate_api = replicate_api.Client(api_token=os.environ["REPLICATE_API_TOKEN"])
     else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        replicate_api = st.text_input('Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+            st.warning('Insira suas credencias ', icon='⚠️')
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+            st.success('Pode falar com o bot!', icon='👉')
 
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    #llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
 
-# Store LLM generated responses
+# Guarda o historico do chat LLM
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "Assistant", "content": "Como você está se sentindo hoje??"}]
 
-# Display or clear chat messages
+# Mostrar ou limpar as a conversa
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    st.session_state.messages = [{"role": "Assistant", "content": "Como você está se sentindo hoje??"}]
+st.sidebar.button('Limpar chat', on_click=clear_chat_history)
 
-# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+# Fução para gerar a resposta
 def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    string_dialogue = "Você é um psicólogo brasileiro que tenta ajudar os usuários com problemas ou sentimentos que os incomodam com conselhos, sempre em portugues. Você não responde como 'User' nem finge ser 'User'. Você só uma vez quando for 'Assistant'."
     for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
+        if dict_message["role"] == "User":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                            #'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea',
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+                                  "temperature":0.1, "top_p":0.9, "max_length":120, "repetition_penalty":1})# temperatura bem baixa para o modelo nao correr riscos e dar uma resposta menos criativa
     return output
 
-# User-provided prompt
+# Prompt do usuario
 if prompt := st.chat_input(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    st.session_state.messages.append({"role": "User", "content": prompt})
+    with st.chat_message("User"):
         st.write(prompt)
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
+# Gera a resposta se a ultima msg nao for do Assistant
+if st.session_state.messages[-1]["role"] != "Assistant":
+    with st.chat_message("Assistant"):
+        with st.spinner("Pensando..."):
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
             for item in response:
                 full_response += item
                 placeholder.markdown(full_response)
+                full_response = GoogleTranslator(source='auto', target='pt').translate(full_response)
             placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
+            full_response = GoogleTranslator(source='auto', target='pt').translate(full_response)    
+    
+    # Traduzindo a resposta para português
+    
+    message = {"role": "Assistant", "content": full_response}
     st.session_state.messages.append(message)
